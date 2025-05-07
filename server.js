c57d3570a0614a974de4ce4614cf9114943f1f55
Shruti Joshi <132460048+shrutij22@users.noreply.github.com>
2025-05-07 08:23:51 +0530

    Add files via upload
    WAD 5

---
+++ server.js
@@ -0,0 +1,16 @@
+// Import the 'http' module
+const http = require('http');
+
+// Create a server
+const server = http.createServer((req, res) => {
+    // Set response header
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    
+    // Send a response
+    res.end('Hello, this is a Node.js HTTP Server!');
+});
+
+// Server listens on port 3000
+server.listen(3000, () => {
+    console.log('Server is running at http://localhost:3000');
+});
